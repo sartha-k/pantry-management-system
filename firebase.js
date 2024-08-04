0c391f36262fa95cc3911e1a4a94b94625f6bafe
sarthak <sarthaksharma661@gmail.com>
2024-08-04 21:08:08 +0530

    Fix Firebase Analytics initialization for server-side rendering

--- firebase.js
+++ firebase.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBP9HVmP0gs8l4w9Cxk-8hG_2YDsznGV7U",
   authDomain: "inventort-managment.firebaseapp.com",
@@ -19,6 +16,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore(app)
-export {firestore}
+const firestore = getFirestore(app);
+
+let analytics;
+if (typeof window !== 'undefined') {
+  // Only initialize Analytics if on the client-side
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+
+export { firestore, analytics };
